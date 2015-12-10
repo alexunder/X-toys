66c1d70ad3499a966dac4cebe2d563935b0e79db
Alexander Hsu <alexanderxu@asrmicro.com>
2015-12-10 18:09:49 +0800

    continue

--- graphic/assighment2/Plane.h
+++ graphic/assighment2/Plane.h
@@ -14,7 +14,11 @@ class Plane : public Object3D
 {
 public:
     Plane(Vec3f &normal, float d, Material *m);
+    Plane(Vec3f a, Vec3f b, Vec3f c, Material *m00);
+    bool intersect(const Ray &r, Hit &h, float tmin);
 private:
+    Vec3f mNormal;
+    float mDistance;
 };
 
 #endif
