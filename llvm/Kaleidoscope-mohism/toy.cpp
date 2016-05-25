e6bea803df5f0cb1da4ac74e3be0465a2b05276d
Alexander Hsu <alexanderxu@asrmicro.com>
2016-05-25 19:32:46 +0800

    fucking llvm

--- llvm/Kaleidoscope-mohism/toy.cpp
+++ llvm/Kaleidoscope-mohism/toy.cpp
@@ -10,16 +10,18 @@
 #include <string>
 #include <vector>
 
-namespace helper {
+namespace helper
+{
 //Cloning make_unique here until it's standard in C++ 14
 //Using a namespace to avoid confliting with MSVC's std::make_unique (which
 // ADL can soemtimes find in unqualified calls).
-template <class T, class... Args>
-static 
-    typename std::enable_if<!std::is_array<T>::value, std::unique_ptr<T>>::type
-    make_unique(Args &&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
+    template <class T, class... Args>
+    static 
+        typename std::enable_if<!std::is_array<T>::value, std::unique_ptr<T>>::type
+        make_unique(Args &&... args)
+    {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
 } // end namespace helper
 
 //
@@ -29,7 +31,9 @@ static
 //The lexer returns tolens [0-255] if it is an unknown chraractor,
 //otherwise one of these for known things.
 
-enum Token {
+enum Token
+{
+    tok_err = 0,
     tok_eof = -1,
 
     // commands
@@ -41,4 +45,96 @@ enum Token {
     tok_number = -5
 };
 
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;             // Filled in if tok_number
 
+/// gettok - Return the next token from standard input
+
+static int gettok()
+{
+    static int LastChar = ' ';
+
+    //Skip any whitespace.
+    while (isspace(LastChar))
+        LastChar = getchar();
+
+    //identifier: [a-zA-Z][a-zA-Z0-9]
+    if (isalpha(LastChar))
+    {
+        IdentifierStr = LastChar;
+
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        if (IdentifierStr == "def")
+            return tok_def;
+
+        if (IdentifierStr == "extern")
+            return tok_extern;
+
+        return tok_identifier;
+    }
+
+    // Number: [0-9.]
+    if (isdigit(LastChar) || LastChar == '.')
+    {
+        std::string NumStr;
+        bool isDotShown = false;
+        int countDot = 0;
+
+        do
+        {
+            if (LastChar == '.')
+            {
+                countDot++; 
+            }
+
+            if (countDot > 1)
+                return tok_err;
+
+            NumStr += LastChar;
+            LastChar = getchar();
+        }
+        while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), nullptr);
+        return tok_number;
+    }
+
+    // Comment until end of line
+    if (LastChar == '#')
+    {
+        do
+            LastChar = getchar();
+        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF)
+            return gettok();
+    }
+
+    // Check for end of file. Don't eat the EOF.
+    if (LastChar == EOF)
+        return tok_eof;
+
+    // Otherwise, just return the charactor as its ascii value
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
+
+
+//
+// Abstract Syntax Tree
+//
+
+namespace
+{
+/// ExprAST - Base class for all expression nodes
+    class ExprAST
+    {
+    public:
+        virtual ~ExprAST()
+        {
+        }
+    };
+}
