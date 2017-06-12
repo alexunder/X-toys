290d58bf70ab8a4322df7047abbbb698482a6327
Alexander Hsu <alexanderxu@asrmicro.com>
2017-06-12 19:09:13 +0800

    continue

--- algorithms/percolation/Percolation.java
+++ algorithms/percolation/Percolation.java
@@ -1,7 +1,7 @@
 public class Percolation {
     // create n-by-n grid, with all sites blocked
     public Percolation(int n)  {
-        mData = new [n][n];
+        mData = new int[n][n];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 mData[i][j] = 0;
@@ -9,19 +9,52 @@ public class Percolation {
     }
     // open site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (row < 1 || row > Num)
-            return;
-        if (col < 1 || col > Num)
-            return;
-        mData[row - 1][col - 1] = 1;
+        if (row < 1 || row > mNum)
+            throw new IndexOutOfBoundsException();
+        if (col < 1 || col > mNum)
+            throw new IndexOutOfBoundsException();
+
+        if (mData[row - 1][col - 1] == 0)
+            mData[row - 1][col - 1] = 1;
     }
-    public boolean isOpen(int row, int col)  // is site (row, col) open?
-    public boolean isFull(int row, int col)  // is site (row, col) full?
-    public     int numberOfOpenSites()       // number of open sites
-    public boolean percolates()              // does the system percolate?
-    public static void main(String[] args)   // test client (optional)
-private:
-    int mData[][];
-    int mNum;
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (row < 1 || row > mNum)
+            throw new IndexOutOfBoundsException();
+        if (col < 1 || col > mNum)
+            throw new IndexOutOfBoundsException();
+        return (mData[row - 1][col - 1] == 1);
+    }
+    // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (row < 1 || row > mNum)
+            throw new IndexOutOfBoundsException();
+        if (col < 1 || col > mNum)
+            throw new IndexOutOfBoundsException();
+        return (mData[row - 1][col - 1] == 2);
+    }
+    // number of open sites
+    public int numberOfOpenSites() {
+        int i;
+        int j;
+        int num_open_sites = 0;
+        for (i = 0; i < mNum; i++)
+            for (j = 0; j < mNum; j++) {
+            if (mData[i][j] != 0)
+                num_open_sites++;
+        }
+        return num_open_sites;
+    }
+    // does the system percolate?
+    public boolean percolates() {
+       return false;
+    }
+    // test client (optional)
+    public static void main(String[] args) {
+        return;
+    }
+
+    private int mData[][];
+    private  int mNum;
 }
 
